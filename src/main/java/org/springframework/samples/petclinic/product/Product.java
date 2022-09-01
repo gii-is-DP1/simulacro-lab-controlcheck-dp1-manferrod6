a38c83f45a17aae331aa841e6dad160efb095f7b
Manuel Fernández Rodríguez <manferrod6@alum.us.es>
2022-09-01 10:24:54 +0200

    Commit hasta el punto 4

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,38 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
+@Entity
+@Table(name="products")
+public class Product extends BaseEntity{
+	
+	@NotNull
+	@Size(min=3, max=50)
+	@Column(name="name")
     String name;
+	
+	@Min(0)
+	@NotNull
+	@Column(name= "price")
     double price;
+	
+	@ManyToOne
+	@NotNull
+	@JoinColumn(name="product_type")
     ProductType productType;
+	
 }
